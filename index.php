5a3c626ec1996f103bf1869b90ed8ec36c3e84c3
Vera de Kok <veradekok@gmail.com>
2013-10-13 20:18:21 +0200

    +status_to_color SQL table

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header( "Content-type: image/svg+xml" );
 ?>
 <!-- Creator: Esri ArcMap 10.2.0.3348 -->
 <svg width="549.75118pt" height="633.00472pt" viewBox="0 0 549.75118 633.00472" enable-background="new 0 0 549.75118 633.00472"
-  version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" author="Centraal Bureau voor de Statistiek en het Kadaster" >
+  version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" mapauthor="Centraal Bureau voor de Statistiek en het Kadaster" >
   <g id="Layers">
     <g id="gem_2012_v1">
       <clipPath id="SVG_CP_1">
@@ -13,34 +13,38 @@ header( "Content-type: image/svg+xml" );
 <?php
 
 // Create connection
-$con=mysqli_connect("localhost","root","","test");
+// $con=mysqli_connect("localhost","root","","test");
 // Check connection
 if (mysqli_connect_errno($con)) { echo "Failed to connect to MySQL: " . mysqli_connect_error(); }
 
 
-$result = mysqli_query($con,"SELECT _progress.*, _cbs_changes.2012_cbs FROM _progress LEFT JOIN _cbs_changes ON _progress.gemeentenummer = _cbs_changes.new_cbs");
+$result = mysqli_query($con,"SELECT _progress.*, _cbs_changes.2012_cbs FROM _progress LEFT JOIN _cbs_changes ON _progress.cbs_nr = _cbs_changes.new_cbs");
 
 //create results array
 while($row = mysqli_fetch_array($result)){
   if ($row["2012_cbs"] == NULL){
-      $colors[$row['gemeentenummer']] = $row['status'];
+      $gm_status[$row['cbs_nr']] = $row['status'];
   }
   else{
-      $colors[$row['2012_cbs']] = $row['status'];
+      $gm_status[$row['2012_cbs']] = $row['status'];
   }  
 }
 
-//loop through SVG XML, replace colors where needed  
+$result = mysqli_query($con,"SELECT * FROM _status_to_color");
+
+//create results array
+while($row = mysqli_fetch_array($result)){
+  $colors[$row['status']]  = $row['color'];
+  
+}
+
+
+//loop through SVG XML, replace color where needed  
 $xml = simplexml_load_file("map.xml");
 
 foreach($xml->children() as $child){
-    switch ($colors[(string)$child['cbs']]){
-      case "2":
-        $child['fill'] = '#99141b';
-        break;
-      case "1":
-        $child['fill'] = '#9C6769';
-        break;
+    if(isset($colors[$gm_status[(string)$child['cbs']]])){
+      $child['fill'] = "#".$colors[$gm_status[(string)$child['cbs']]];
     } 
     createRow($child['clip-path'], $child['fill'], $child['gem'], $child['cbs'], $child['d'] );
   }
@@ -49,7 +53,7 @@ foreach($xml->children() as $child){
 
 function createRow ($clippath, $fill, $gem, $cbs, $d ){
  echo '
- <path clip-path="'.$clippath.'" fill="'.$fill.'" gem="'.$gem.'" cbs="'.$cbs.'" fill-rule="evenodd" stroke="#6E6E6E" stroke-width="0.48" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="'.$d.'" />';
+ <path clip-path="'.$clippath.'" fill="'.$fill.'" gem="'.$gem.'" cbs="'.$cbs.'" fill-rule="evenodd" stroke="#928b91" stroke-width="0.48" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="'.$d.'" />';
 } 
 ?>
     </g>
